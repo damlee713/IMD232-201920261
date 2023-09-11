27ee71973677633ef77a206f1c656249c534a4c0
damlee713 <damlee713@gmail.com>
2023-09-11 16:02:20 +0900

    fuck

---
+++ src/w2/sketches/sketch2/script/alertpromoptconfirm.js
@@ -0,0 +1,3 @@
+alert('Hello World');
+let usertype = prompt("What's your name?", 'Hong');
+confirm('Is your name ' + usertype + '?');
---
+++ src/w2/sketches/sketch4/script/scech.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+  circle(mouseX, mouseY, 100);
+}
+src / w2 / sketches / sketch4 / script / scech.js;
